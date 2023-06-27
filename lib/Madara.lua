21d3110cc8280cb257809afddb4032ed9df8a52e
X-Noid <xgorntv@gmail.com>
2023-06-27 14:44:17 +0900

    Update Madara.lua

--- lib/Madara.lua
+++ lib/Madara.lua
@@ -132,9 +132,12 @@ function defaults:getPassage(url)
 	-- Remove/modify unwanted HTML elements to get a clean webpage.
 	htmlElement:select("div.lnbad-tag"):remove() -- LightNovelBastion text size
 	htmlElement:select("i.icon.j_open_para_comment.j_para_comment_count"):remove() -- BoxNovel, VipNovel numbers
+
+	-- Convert element to string
+	local stringElement = tostring(htmlElement)
 	
 	-- Translate text
-	local translatedText = RequestDocument(POST("https://api.xgorn.tech/translator", nil, RequestBody(qs({ text=tostring(htmlElement) }), MediaType("application/x-www-form-urlencoded")))):selectFirst("div.text")
+	local translatedText = RequestDocument(POST("https://api.xgorn.tech/translator", nil, RequestBody(qs({ text=stringElement }), MediaType("application/x-www-form-urlencoded")))):selectFirst("div.text")
 	
 	-- Insert title
 	translatedText:prepend("<h1>" .. title .. "</h1>");

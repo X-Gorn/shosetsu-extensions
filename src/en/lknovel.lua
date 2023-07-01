d03eda0b7dde4532681c4a260b79b24ac8cbba86
X-Noid <xgorntv@gmail.com>
2023-07-02 05:23:34 +0900

    Update lknovel.lua

--- src/en/lknovel.lua
+++ src/en/lknovel.lua
@@ -1,5 +1,35 @@
 -- {"id":75,"ver":"1.0.6","libVer":"1.0.0","author":"Doomsdayrs, AbhiTheModder","dep":["Madara>=2.2.0"]}
 
+--- Internal settings store.
+---
+--- Completely optional.
+---  But required if you want to save results from [updateSetting].
+---
+--- Notice, each key is surrounded by "[]" and the value is on the right side.
+local settings = {
+	[1] = "test",
+	[2] = false,
+}
+
+--- Settings model for Shosetsu to render.
+---
+--- Optional, Default is empty.
+---
+local settingsModel = {
+	TextFilter(1, "API Key"),
+	SwitchFilter(2, "RANDOM SWITCH INPUT"),
+}
+
+
+--- Called when a user changes a setting and when the extension is being initialized.
+---
+--- Optional, But required if [settingsModel] is not empty.
+---
+local function updateSetting(id, value)
+	settings[id] = value
+end
+
+
 return Require("Madara")("https://lknovel.com", {
   id = 325,
   name = "Lknovel",
@@ -7,7 +37,8 @@ return Require("Madara")("https://lknovel.com", {
   imageURL = "https://lknovel.com/wp-content/uploads/2017/10/lknovelljljl.png",
   chaptersScriptLoaded = true,
   hasSearch = true,
-
+  settings = settingsModel,
+  updateSetting = updateSetting,
   genres = {
     "Action",
     "Adventure",

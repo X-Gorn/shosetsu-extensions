80b6086ba9536049245f6d3461fdcf9384b8c730
X-Noid <xgorntv@gmail.com>
2023-07-02 05:57:32 +0900

    Update lknovel.lua

--- src/en/lknovel.lua
+++ src/en/lknovel.lua
@@ -10,8 +10,8 @@ local RANDOM_SWITCH_INPUT = 202
 ---
 --- Notice, each key is surrounded by "[]" and the value is on the right side.
 local settings = {
-	[API_KEY] = "test",
-    [RANDOM_SWITCH_INPUT] = false,
+	    [API_KEY] = "test",
+        [RANDOM_SWITCH_INPUT] = false,
 }
 
 --- Settings model for Shosetsu to render.
@@ -19,8 +19,8 @@ local settings = {
 --- Optional, Default is empty.
 ---
 local settingsModel = {
-	TextFilter(API_KEY, "API Key"),
-    SwitchFilter(RANDOM_SWITCH_INPUT), "RANDOM SWITCH INPUT",
+	    TextFilter(API_KEY, "API Key"),
+        SwitchFilter(RANDOM_SWITCH_INPUT), "RANDOM SWITCH INPUT",
 }
 
 
@@ -34,45 +34,45 @@ end
 
 
 return Require("Madara")("https://lknovel.com", {
-  id = 325,
-  name = "Lknovel",
-  latestNovelSel = "div.col-6.col-md-3.badge-pos-1",
-  imageURL = "https://lknovel.com/wp-content/uploads/2017/10/lknovelljljl.png",
-  chaptersScriptLoaded = true,
-  hasSearch = true,
-  settings = settingsModel,
-  updateSetting = updateSetting,
-  genres = {
-    "Action",
-    "Adventure",
-    "Comedy",
-    "Drama",
-    "Ecchi",
-    "Fantasy",
-    "Gender Bender",
-    "Harem",
-    "Historical",
-    "Horror",
-    "Josei",
-    "Martial Arts",
-    "Mature",
-    "Mecha",
-    "Mystery",
-    "Psychological",
-    "Romance",
-    "School Life",
-    "Sci-fi",
-    "Seinen",
-    "Shoujo",
-    "Shounen",
-    "Slice of Life",
-    "Smut",
-    "Sports",
-    "Supernatural",
-    "Tragedy",
-    "Wuxia",
-    "Xianxia",
-    "Xuanhuan",
-    "Yaoi"
-  }
+    id = 325,
+    name = "Lknovel",
+    latestNovelSel = "div.col-6.col-md-3.badge-pos-1",
+    imageURL = "https://lknovel.com/wp-content/uploads/2017/10/lknovelljljl.png",
+    chaptersScriptLoaded = true,
+    hasSearch = true,
+    settings = settingsModel,
+    updateSetting = updateSetting,
+    genres = {
+        "Action",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Ecchi",
+        "Fantasy",
+        "Gender Bender",
+        "Harem",
+        "Historical",
+        "Horror",
+        "Josei",
+        "Martial Arts",
+        "Mature",
+        "Mecha",
+        "Mystery",
+        "Psychological",
+        "Romance",
+        "School Life",
+        "Sci-fi",
+        "Seinen",
+        "Shoujo",
+        "Shounen",
+        "Slice of Life",
+        "Smut",
+        "Sports",
+        "Supernatural",
+        "Tragedy",
+        "Wuxia",
+        "Xianxia",
+        "Xuanhuan",
+        "Yaoi"
+    }
 })

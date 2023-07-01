1d98474e0df35b772a5efb99f966dd7181669224
X-Noid <xgorntv@gmail.com>
2023-07-01 11:48:51 +0900

    Add Lknovel

---
+++ src/en/lknovel.lua
@@ -0,0 +1,44 @@
+-- {"id":75,"ver":"1.0.6","libVer":"1.0.0","author":"Doomsdayrs, AbhiTheModder","dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://lknovel.com", {
+  id = 325,
+  name = "Lknovel",
+  latestNovelSel = "div.col-6.col-md-3.badge-pos-1",
+  imageURL = "https://lknovel.com/wp-content/uploads/2017/10/lknovelljljl.png",
+  chaptersScriptLoaded = true,
+  hasSearch = true,
+
+  genres = {
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Ecchi",
+    "Fantasy",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Martial Arts",
+    "Mature",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi"
+  }
+})

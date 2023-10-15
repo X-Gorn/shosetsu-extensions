7fb42de9aa7994d9e0a19cc092477a525b5df3e9
X-Gorn <xgorntv@gmail.com>
2023-10-15 15:35:00 +0900

    fix: ReadLightNovel

--- src/en/ReadLightNovel.lua
+++ src/en/ReadLightNovel.lua
@@ -63,10 +63,14 @@ return {
 		-- Remove/modify unwanted HTML elements to get a clean webpage.
 		htmlElement:select("br"):remove() -- Between each <p> is a <br>.
 
-		local elementString = tostring(htmlElement)
-		local translatedText = RequestDocument(POST("https://api.xgorn.pp.ua/translator", nil,
-			RequestBody(qs({ text = elementString }), MediaType("application/x-www-form-urlencoded")))):selectFirst(
-			"div.text")
+		local elementString = tostring(chapter)
+		local res = RequestDocument(POST("https://api.xgorn.pp.ua/translate/html", nil,
+			FormBodyBuilder()
+			:add("lang", "Indonesian")
+			:add("html_text", elementString):build()
+		))
+		local raw_html = json.decode(res:toString():sub(33, -18))
+		local translatedText = Document(raw_html.html_text)
 		translatedText:child(0):before("<h1>" .. title .. "</h1>");
 		return pageOfElem(translatedText)
 	end,
